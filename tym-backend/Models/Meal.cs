a371711ce6d64e4244d16b07253d3188f14d6a27
P1wko <miminios20611@gmail.com>
2024-10-04 20:11:16 +0200

    Basic GET method for API

---
+++ tym-backend/Models/Meal.cs
@@ -0,0 +1,10 @@
+﻿namespace tym_backend.Models
+{
+	public class Meal
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public string Type { get; set; }
+	}
+}
---
+++ tym-backend/Models/backendContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace tym_backend.Models
+{
+	public class backendContext : DbContext
+	{
+		public backendContext(DbContextOptions<backendContext> options) : base(options) { }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+		}
+
+
+		public DbSet<Meal> Meals { get; set; }
+	}
+}
