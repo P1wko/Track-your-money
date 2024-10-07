d896c17837f820e72c19a0afdd215a93ad280c52
P1wko <miminios20611@gmail.com>
2024-10-07 15:54:49 +0200

    FE: styles for footer and navbar

--- tym-frontend/src/app/dashboard/footer.tsx
+++ tym-frontend/src/app/dashboard/footer.tsx
@@ -5,9 +5,24 @@ const EMAIL = "miminios20611@gmail.com"
 
 export default function Footer() {
     return (
-        <footer>
-            <p>{NAME}</p>
-            <p>E-mail:<Link href="mailto:miminios20611@gmail.com">{EMAIL}</Link></p>
+        <footer className="bg-blue-800 text-gray-300 py-8">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="flex justify-between">
+                    <div>
+                        <p>{NAME}</p>
+                        <p>E-mail:<Link href="mailto:miminios20611@gmail.com">{EMAIL}</Link></p>
+                    </div>
+                    <div>
+                        <h2 className="text-lg font-semibold text-white">Follow me</h2>
+                        <div className="mt-4 flex space-x-4">
+                            <a href="#" className="hover:text-white">Facebook</a>
+                            <a href="#" className="hover:text-white">Twitter</a>
+                            <a href="#" className="hover:text-white">Instagram</a>
+                            <a href="#" className="hover:text-white">LinkedIn</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </footer>
     )
 }
