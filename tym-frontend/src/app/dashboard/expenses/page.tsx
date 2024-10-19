12af12b5546ba8096a009aef18b0c73807456a93
P1wko <miminios20611@gmail.com>
2024-10-19 12:19:00 +0200

    FE: displaying basic modal for adding expenses

--- tym-frontend/src/app/dashboard/expenses/page.tsx
+++ tym-frontend/src/app/dashboard/expenses/page.tsx
@@ -1,6 +1,10 @@
+"use client"
+
 import { PlusCircleIcon } from "@heroicons/react/24/outline"
+import { useState } from "react"
 
 export default function Expenses() {
+    const [showCreateForm, setShowCreateForm] = useState(false)
     const dummyData = [
         {
             id: 1,
@@ -16,11 +20,44 @@ export default function Expenses() {
         }
     ]
 
+    const toggleCreateForm = () => setShowCreateForm(!showCreateForm);
+
     return (
         <div className="flex-1 p-8 bg-gray-100">
+           {showCreateForm && (
+                <div className="fixed inset-0 bg-gray-900 bg-opacity-50 flex justify-center items-center z-50">
+                    <div className="bg-white p-8 rounded-lg shadow-lg w-96">
+                        <h2 className="text-2xl font-bold mb-4">Formularz</h2> 
+                        <form>
+                            <div className="mb-4">
+                                <label className="block text-gray-700 text-sm font-bold mb-2">
+                                Name
+                                </label>
+                                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Your Name" />
+                            </div>
+
+                            <div className="mb-4">
+                                <label className="block text-gray-700 text-sm font-bold mb-2">
+                                Email
+                                </label>
+                                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="email" placeholder="Your Email" />
+                            </div>
+
+                            <div className="flex justify-end">
+                                <button type="button" onClick={toggleCreateForm} className="bg-red-500 text-white p-2 rounded-lg hover:bg-red-700">
+                                Cancel
+                                </button>
+                                <button type="submit" className="ml-4 bg-blue-500 text-white p-2 rounded-lg hover:bg-blue-700">
+                                Submit
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            )}
             <div className="flex justify-between items-center mb-4">
                 <h1 className="text-2xl font-bold">Expenses List</h1>
-                <button className="flex items-center bg-gray-800 rounded-md p-3 text-white shadow-lg hover:bg-gray-700">
+                <button className="flex items-center bg-gray-800 rounded-md p-3 text-white shadow-lg hover:bg-gray-700" onClick={toggleCreateForm}>
                     <PlusCircleIcon className="h-6 w-6 mr-2" aria-hidden="true" />
                     Create New
                 </button>
